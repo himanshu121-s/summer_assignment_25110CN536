e3817e133dc087c9e6f7bade299141058f50a6f0
himanshu <himanshusingh79161@gmail.com>
2026-06-07 10:24:50 +0530

    day 2 question 1 completed

---
+++ day_2/sum_of_n.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n,d, sum=0;
+    printf(" enter the number:");
+    scanf("%d",&n);
+    while(n>0){
+        d= n%10;
+        sum= sum+d;
+        n=n/10;
+    } printf( " the sum of entered number is:%d", sum);
+    return 0;
+}
