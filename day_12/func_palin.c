60a0430239c22d585f461db58e54f64bc1baa03e
himanshu <himanshusingh79161@gmail.com>
2026-06-25 11:20:27 +0530

    day_12_completed

---
+++ day_12/func_palin.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int palindrome(int n)
+{
+    int temp = n, rev = 0, rem;
+
+    while (n != 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;
+    }
+
+    if (temp == rev)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (palindrome(num))
+        printf("%d is a Palindrome number.\n", num);
+    else
+        printf("%d is not a Palindrome number.\n", num);
+
+    return 0;
+}
