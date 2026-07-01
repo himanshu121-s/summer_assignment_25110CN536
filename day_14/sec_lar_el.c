952c94a4224d6668e6034e2eca2f93950bfc87c7
himanshu <himanshusingh79161@gmail.com>
2026-07-01 09:25:05 +0530

    day_14_completed

---
+++ day_14/sec_lar_el.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], n, i;
+    int largest, secondLargest;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    largest = secondLargest = arr[0];
+
+    for(i = 1; i < n; i++)
+    {
+        if(arr[i] > largest)
+        {
+            secondLargest = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > secondLargest && arr[i] != largest)
+        {
+            secondLargest = arr[i];
+        }
+    }
+
+    printf("Largest element = %d\n", largest);
+    printf("Second largest element = %d\n", secondLargest);
+
+    return 0;
+}
